e55002a1912119091d1603a3dd3fe3ca8e466d2c
Bassel Amer <Bassel_Victus@hotmail.com>
2026-04-02 00:30:39 +0300

    Update webservices.dart

--- lib/features/homescreen/data/webservices.dart
+++ lib/features/homescreen/data/webservices.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 
 import 'package:newsapp/core/consts/consts.dart';
 
+String yourAPIKey='';
+
 class Webservices {
   late Dio dio;
 
@@ -34,7 +36,7 @@ class Webservices {
         'top-headlines',
         queryParameters: {
           "country": country,
-          "apiKey": "9bd498111e3d402fb2103e2dc16d9e92",
+          "apiKey": yourAPIKey,
           'category': category,
         },
       );
@@ -51,37 +53,3 @@ class Webservices {
 
 
 
-// Singleton Pattern //
-
-// class Webservices {
-//   // 1. The Private Static Instance
-//   // This holds the ONE true copy of your Webservices in memory.
-//   static final Webservices _instance = Webservices._internal();
-
-//   late Dio dio;
-
-//   // 2. The Factory Constructor
-//   // Whenever ANY file in your app calls Webservices(), it doesn't create a new one.
-//   // It just hands them the '_instance' we created above.
-//   factory Webservices() {
-//     return _instance;
-//   }
-
-//   // 3. The Private Internal Constructor
-//   // The underscore (_) means "private". This block of code only ever runs exactly ONCE.
-//   Webservices._internal() {
-//     BaseOptions options = BaseOptions(
-//       baseUrl: 'https://newsapi.org/v2/',
-//       receiveDataWhenStatusError: true,
-//       connectTimeout: const Duration(seconds: 20),
-//       receiveTimeout: const Duration(seconds: 20),
-//     );
-
-//     dio = Dio(options);
-
-//     dio.interceptors.add(LogInterceptor(
-//       responseBody: true,
-//       error: true,
-//       requestHeader: false,
-//     ));
-//   }
